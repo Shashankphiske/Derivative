f3fb60f48859ab58edfb4ac4068e861948758fe6
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-07 22:45:20 +0530

    Update CIAmaths.py

--- CIAmaths.py
+++ CIAmaths.py
@@ -9,3 +9,12 @@ import matplotlib.pyplot as plot
 from scipy.misc import derivative
 
 x,a,b,c = smp.symbols('a,b,c,x' , real=True) # Here we defined our variables we are going to use 
+# its always good to define this symbols and we are saying that these are real variables/values
+# you can do multiple operation on these now and it will be outputed as symbolic expressions
+
+#smp.diff(x,y,n) :this is the function used to differentiate where x is the function which we want to
+#differentiate and y is the function we are differentiating it with respect to and n is the number
+#how many times you need the derivative 2nd,third etc
+
+# the derivative created can be used to derive solutions by plugging in values using 
+# eg: derivativecreated.subs([x,4],[a,2],[b,3],[c,5]).evalf() it returns float value
